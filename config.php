eb0abdcaa0819011810feba3daa192f26400dd97
Andrew Chamamme <mastys100@gmail.com>
2018-06-20 14:18:36 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+$config = [
++    'driver' => 'mysqli',
++    'server' => "localhost",
++    'username' => "root",
++    'password' => "",
++    'port' => "3306",
++    'database' => "omvc",
++    'debug' => true
++];
